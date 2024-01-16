da81c1371da599ad648c01854a6b2dbf1244aace
Daniel Joseph <daniel.joseph@cashcall.com.eg>
2024-01-16 14:27:53 +0200

    adding social media platforms urls

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+final Uri linkedin_url = Uri.parse('https://www.linkedin.com/in/daniel-joseph-b04391204/');
+final Uri facebook_url = Uri.parse('https://www.facebook.com/profile.php?id=100002270578896');
+final Uri instagram_url = Uri.parse('https://www.instagram.com/danial_joseph11/');
 
 void main() {
   runApp(const MyApp());
@@ -49,65 +56,112 @@ class MyApp extends StatelessWidget {
               const SizedBox(
                 height: 30,
               ),
-              Container(
+              Card(
                 color: Colors.white,
-                width: double.infinity,
-                height: 50,
-                margin: const EdgeInsets.only(left: 40, right: 40),
-                child:  Center(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(left: 15),
-                        child: const Icon(Icons.phone,
-                          color: Colors.teal,
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 45),
-                        child: const Text(
-                          '01140114441',
-                          style: TextStyle(color: Colors.teal,
-                            fontSize: 19,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 5,
+                margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(left: 15),
+                          child: const Icon(
+                            Icons.phone,
+                            color: Colors.teal,
                           ),
                         ),
-                      ),
-                    ],
+                        Container(
+                          margin: EdgeInsets.only(left: 45),
+                          child: const Text(
+                            '01140114441',
+                            style: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 5,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
               const SizedBox(
-                height: 30,
+                height: 10,
               ),
-              Container(
+              Card(
                 color: Colors.white,
-                width: double.infinity,
-                height: 50,
-                margin: const EdgeInsets.only(left: 40, right: 40),
-                child:  Center(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(left: 15),
-                        child: const Icon(Icons.email,
-                          color: Colors.teal,
+                margin:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(left: 15),
+                          child: const Icon(
+                            Icons.email,
+                            color: Colors.teal,
+                          ),
                         ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 20),
-                        child: const Text(
-                          'danielazmy541@gmail.com',
-                          style: TextStyle(color: Colors.teal,
-                          fontSize: 19,
-                          fontWeight: FontWeight.bold,),
+                        Container(
+                          margin: EdgeInsets.only(left: 20),
+                          child: const Text(
+                            'danielazmy541@gmail.com',
+                            style: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
+               const SizedBox(
+                 height: 20,
+               ),
+               Container(
+                 margin: EdgeInsets.only(left: 100),
+                 child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: GestureDetector(
+                        onTap: _launchUrl,
+                        child: const Image(image: AssetImage('images/LinkedIn.svg'),
+                          height: 50,
+                          width: 50,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 30,),
+                    Container(
+                      child: GestureDetector(
+                        onTap: _facebook_launchUrl,
+                        child: const Image(image: AssetImage('images/facebook.png'),
+                          height: 50,
+                          width: 50,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 30,),
+                    Container(
+                      child: GestureDetector(
+                        onTap: _instagram_launchUrl,
+                        child: const Image(image: AssetImage('images/instagram.svg'),
+                          height: 50,
+                          width: 50,
+                        ),
+                      ),
+                    ),
+                  ],
+                               ),
+               )
             ],
           ),
         )),
@@ -115,3 +169,20 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+Future<void> _launchUrl() async {
+  if (!await launchUrl(linkedin_url)) {
+    throw Exception('Could not launch $linkedin_url');
+  }
+}
+Future<void> _facebook_launchUrl() async {
+  if (!await launchUrl(facebook_url)) {
+    throw Exception('Could not launch $instagram_url');
+  }
+}
+Future<void> _instagram_launchUrl() async {
+  if (!await launchUrl(instagram_url)) {
+    throw Exception('Could not launch $instagram_url');
+  }
+}
+
+
